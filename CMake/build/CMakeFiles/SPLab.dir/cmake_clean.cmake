58e14425477d694b1e5cddb66fd89f165378b295
Garnik645 <garnik645@gmail.com>
2021-10-09 17:14:55 +0400

    CMake files were added with a project, containing main.cpp main file, gcd.h and gcd.cpp files calculating gcd of two numbers, where .cpp files are compiled and linked together

---
+++ CMake/build/CMakeFiles/SPLab.dir/cmake_clean.cmake
@@ -0,0 +1,10 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/SPLab.dir/main.cpp.o"
+  "SPLab"
+  "SPLab.pdb"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/SPLab.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
---
+++ CMake/main.cpp
@@ -0,0 +1,7 @@
+#include <iostream>
+#include "gcd.h"
+
+int main()
+{
+	std::cout << gcd(75, 90) << '\n';
+}
