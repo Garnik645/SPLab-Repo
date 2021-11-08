7a8916b9fa6f110986b24b7af6e198caaf421a67
Garnik Khroyan <garnik645@gmail.com>
2021-11-08 16:54:32 +0000

    in find_path.cpp comments were removed

--- Graph/find_path.cpp
+++ Graph/find_path.cpp
@@ -8,29 +8,17 @@
 struct Path;
 struct Node;
 
-// structure representing nodes in graph
 struct Node
 {
-    // node index
     size_t index;
-    
-    // edges of the node
     std::vector<Path> edges;
-    
-    // shortest path found from the beginning to this node
     int shortest = std::numeric_limits<int>::max();
-    
-    // previous nodes which came with the shortes path to this node
     std::vector<Node*> prev;
 };
 
-// structure representing paths and edges in graph
 struct Path
 {
-    // distance of the path
     int dist;
-
-    // pointer to end of the path
     Node* node_ptr;
 
     Path(int x = 0, Node* ptr = nullptr) : dist(x), node_ptr(ptr) {} 
@@ -41,10 +29,8 @@ struct Path
     }
 };
 
-// print all shortest paths
 void print_paths(Node* start, Node* current, std::vector<size_t>& path, std::vector<std::string>& sorted)
 {
-    // if the beginning is reached, save the path into the vector 
     if(current == start)
     {
         std::string str = "";
@@ -56,7 +42,6 @@ void print_paths(Node* start, Node* current, std::vector<size_t>& path, std::vec
         sorted.push_back(str);
         return;
     }
-    // recursivly go throw the path from end to start
     for(size_t i = 0; i < current->prev.size(); ++i)
     {
         path.push_back(current->index);
@@ -65,23 +50,19 @@ void print_paths(Node* start, Node* current, std::vector<size_t>& path, std::vec
     }
 }
 
-// Dijkstra's algorithm
 void dijkstra(std::priority_queue<Path, std::vector<Path>, std::greater<Path>>& que)
 {
     Path path = que.top();
     que.pop();
-    // go through all neighbour nodes
     for(size_t i = 0; i < path.node_ptr->edges.size(); ++i)
     {
         Path next_path = path.node_ptr->edges[i];
-        // check if shortest path can be updated
         if(path.node_ptr->shortest + next_path.dist < next_path.node_ptr->shortest)
         {
             next_path.node_ptr->shortest = path.node_ptr->shortest + next_path.dist;
             next_path.node_ptr->prev.clear();
             que.push(Path(next_path.node_ptr->shortest, next_path.node_ptr));
         }
-        // add current node into the vector of previously visited nodes with the shortest distance
         if(path.dist + next_path.dist == next_path.node_ptr->shortest)
         {
             next_path.node_ptr->prev.push_back(path.node_ptr);
@@ -91,33 +72,27 @@ void dijkstra(std::priority_queue<Path, std::vector<Path>, std::greater<Path>>& 
 
 int main(int argc, char** argv)
 {
-    // check if input file is given
     if(argc < 2)
     {
         std::cerr << "Input file path argument is missing\n";
         exit(1);
     }
 
-    // open input file
     std::ifstream input;
     input.open(argv[1]);
     
-    // check for errors while opening the file
     if(input.fail())
     {
         std::cerr << "Something went wrong while opening the file\n";
         exit(1);
     }
     
-    // get number of vertices and edges in graph
     size_t n, m;
     input >> n >> m;
     
-    // create vertices
     std::vector<Node> v(n);
     for(size_t i = 0; i < n; ++i) v[i].index = i;
 
-    // add edges
     for(size_t i = 0; i < m; ++i)
     {
         size_t x, y, z;
@@ -126,11 +101,9 @@ int main(int argc, char** argv)
         v[y].edges.push_back(Path(z, &v[x]));
     }
 
-    // get the begin and end of the path
     size_t begin, end;
     input >> begin >> end;
 
-    // Dijkstra's algorithm
     std::priority_queue<Path, std::vector<Path>, std::greater<Path>> que;
     v[begin].shortest = 0;
     que.push(Path(0, &v[begin]));
@@ -139,15 +112,12 @@ int main(int argc, char** argv)
         dijkstra(que);
     }
 
-    // print shortest distance
     std::cout << "Shortest distance: " << v[end].shortest << std::endl;
     
-    // get all shortest paths
     std::vector<size_t> path;
     std::vector<std::string> sorted;
     print_paths(&v[begin], &v[end], path, sorted);
     
-    // sort all the paths and print in lexicographical order
     std::sort(sorted.begin(), sorted.end());
     for(size_t i = 0; i < sorted.size(); ++i)
     {
