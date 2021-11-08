7ee9aa786261db5fa5a96d0fe2c11fb7bccb470d
Garnik Khroyan <garnik645@gmail.com>
2021-11-08 12:44:53 +0000

    find_path_test.sh was edited

--- Graph/find_path_test.sh
+++ Graph/find_path_test.sh
@@ -3,17 +3,19 @@ DIV=$(dirname $(readlink -f $0))
 run_program() {
     ${DIV}/find_path $1
 }
-g++ find_path.cpp -o find_path
+g++ ${DIV}/find_path.cpp -o ${DIV}/find_path
 for i in {1..2}
 do
-    touch tester.txt
-    run_program "${DIV}/Tests/test${i}.in.txt" > tester.txt
+    touch ${DIV}/tester.txt
+    run_program "${DIV}/Tests/test${i}.in.txt" > "${DIV}/tester.txt"
     if ! cmp -s "${DIV}/tester.txt" "${DIV}/Tests/test${i}.out.txt"
     then
         echo "fail"
+        rm ${DIV}/tester.txt
+        rm ${DIV}/find_path
         exit 1
     fi
-    rm tester.txt
+    rm ${DIV}/tester.txt
 done
-rm find_path
+rm ${DIV}/find_path
 echo "success"
